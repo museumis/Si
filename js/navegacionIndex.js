2afa8bd8ca83532d46841d8e6fca69a5310ac75d
IsmA <IsmA@deLimaMcfly>
2017-07-15 06:29:50 +0200

    Subida por primera vez del código de la página.

---
+++ js/navegacionIndex.js
@@ -0,0 +1,69 @@
+
+function mostrarI(nombreCapa) {
+
+    document.getElementById('listadoIzquierda').style.opacity = "1";
+
+    if (document.getElementById('listadoDerecha').style.opacity) {
+        document.getElementById('listadoDerecha').style.opacity = "0";
+    }
+    document.getElementById(nombreCapa).style.opacity = "1";
+
+}
+;
+
+function mostrarD(nombreCapa) {
+
+    document.getElementById('listadoDerecha').style.opacity = "1";
+    if (document.getElementById('listadoIzquierda').style.opacity) {
+        document.getElementById('listadoIzquierda').style.opacity = "0";
+    }
+    document.getElementById(nombreCapa).style.opacity = "1";
+}
+;
+
+function transicion(nombreCapa) {
+    document.getElementById('listadoIzquierda').style.transition = "all 31s";
+}
+;
+
+//--------------------------------------------------------------------------------------------------------------------
+
+//En esta funcion se ocultan todos los artículos y se muestra el articulo del id que se recibe en la variable articulo;
+function ejecutarArticulos(articulo) {
+    document.querySelector("#mariposa").style.opacity = 0;
+    document.querySelector("#datosPersonales").style.opacity = 0;
+    document.querySelector("#queSe").style.opacity = 0;
+    document.querySelector("#arContacto").style.opacity = 0;
+
+
+
+    document.querySelector(articulo).style.opacity = 1;
+
+
+}
+;
+
+
+window.onload = function () {
+    
+     document.querySelector("#sobreMi").onclick = function () {
+        mostrarI('listadoIzquierda');
+    };
+    
+
+    document.querySelector("#clickPersonal").onclick = function () {
+        ejecutarArticulos("#datosPersonales");
+    };
+
+    document.querySelector("#clickQueSe").onclick = function () {
+        ejecutarArticulos("#queSe");
+
+    };
+    
+    document.querySelector("#clickContacto").onclick = function () {
+        ejecutarArticulos("#arContacto");
+
+    };
+};
+
+
---
+++ js/navegacionJava.js
@@ -0,0 +1,25 @@
+
+//En esta funcion se ocultan todos los artículos y se muestra el articulo del id que se recibe en la variable articulo;
+function ejecutarArticulos(articulo) {
+    document.querySelector("#duke").style.opacity = 0;
+    document.querySelector("#arIf").style.opacity = 0;   
+
+    document.querySelector(articulo).style.opacity = 1;
+
+
+}
+;
+
+
+window.onload = function () {
+
+    document.querySelector("#clickCondicional").onclick = function () {
+        ejecutarArticulos("#arIf");
+    };
+    
+  
+
+
+};
+
+
