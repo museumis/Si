765d015fec88c8c7d6ac3460cedccc4c1a1a7369
IsmA <IsmA@deLimaMcfly>
2017-07-17 08:47:26 +0200

    Subida varios articulos. Ruleta de la fortuna, ahoracado, blackJack y contador de palabras.

--- js/navegacionJava.js
+++ js/navegacionJava.js
@@ -5,16 +5,19 @@ function ejecutarArticulos(articulo) {
     document.querySelector("#arIf").style.opacity = 0;
     document.querySelector("#ar3r").style.opacity = 0;
     document.querySelector("#artemp").style.opacity = 0;
-
+    document.querySelector("#arConT").style.opacity = 0;
+    document.querySelector("#arBlack").style.opacity = 0;
+    document.querySelector("#arAho").style.opacity = 0;
+    document.querySelector("#arRule").style.opacity = 0;
 
     document.querySelector(articulo).style.opacity = 1;
-
-
 }
 ;
 
 
 window.onload = function () {
+
+    ejecutarArticulos("#duke");
 
     document.querySelector("#clickCondicional").onclick = function () {
         ejecutarArticulos("#arIf");
@@ -23,9 +26,25 @@ window.onload = function () {
     document.querySelector("#click3enRaya").onclick = function () {
         ejecutarArticulos("#ar3r");
     };
-    
+
     document.querySelector("#clickTemperatura").onclick = function () {
         ejecutarArticulos("#artemp");
+    };
+
+    document.querySelector("#clickContar").onclick = function () {
+        ejecutarArticulos("#arConT");
+    };
+
+    document.querySelector("#clickBlack").onclick = function () {
+        ejecutarArticulos("#arBlack");
+    };
+
+    document.querySelector("#clickAho").onclick = function () {
+        ejecutarArticulos("#arAho");
+    };
+
+    document.querySelector("#clickRule").onclick = function () {
+        ejecutarArticulos("#arRule");
     };
 
 
