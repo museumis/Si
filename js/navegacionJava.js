f6e15a5a613fb835d6466497fa2101e71a443a85
IsmA <IsmA@deLimaMcfly>
2017-07-17 12:35:30 +0200

    Añadido z-index en el JavaScript de navegacion

--- js/navegacionJava.js
+++ js/navegacionJava.js
@@ -2,15 +2,35 @@
 //En esta funcion se ocultan todos los artículos y se muestra el articulo del id que se recibe en la variable articulo;
 function ejecutarArticulos(articulo) {
     document.querySelector("#duke").style.opacity = 0;
+    document.querySelector("#duke").style.zIndex = 0;
+
+
     document.querySelector("#arIf").style.opacity = 0;
+    document.querySelector("#arIf").style.zIndex = 0;
+
     document.querySelector("#ar3r").style.opacity = 0;
+    document.querySelector("#ar3r").style.zIndex = 0;
+
     document.querySelector("#artemp").style.opacity = 0;
+    document.querySelector("#artemp").style.zIndex = 0;
+
     document.querySelector("#arConT").style.opacity = 0;
+    document.querySelector("#arConT").style.zIndex = 0;
+
+
     document.querySelector("#arBlack").style.opacity = 0;
+    document.querySelector("#arBlack").style.zIndex = 0;
+
     document.querySelector("#arAho").style.opacity = 0;
+    document.querySelector("#arAho").style.zIndex = 0;
+
     document.querySelector("#arRule").style.opacity = 0;
+    document.querySelector("#arRule").style.zIndex = 0;
+
 
     document.querySelector(articulo).style.opacity = 1;
+    document.querySelector(articulo).style.zIndex = 1;
+
 }
 ;
 
