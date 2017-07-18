0df5dbf316ab690c4fa26e1f3c351f1ac0456cf8
IsmA <IsmA@deLimaMcfly>
2017-07-18 14:40:23 +0200

    Responsive Web Design VII. Mobile V.

--- js/navegacionJava.js
+++ js/navegacionJava.js
@@ -1,6 +1,10 @@
 
 //En esta funcion se ocultan todos los artículos y se muestra el articulo del id que se recibe en la variable articulo;
 function ejecutarArticulos(articulo) {
+    
+     document.querySelector("#arConT").style.opacity = 0;
+    document.querySelector("#arConT").style.zIndex = 0;
+    
     document.querySelector("#duke").style.opacity = 0;
     document.querySelector("#duke").style.zIndex = 0;
 
@@ -12,11 +16,7 @@ function ejecutarArticulos(articulo) {
     document.querySelector("#ar3r").style.zIndex = 0;
 
     document.querySelector("#artemp").style.opacity = 0;
-    document.querySelector("#artemp").style.zIndex = 0;
-
-    document.querySelector("#arConT").style.opacity = 0;
-    document.querySelector("#arConT").style.zIndex = 0;
-
+    document.querySelector("#artemp").style.zIndex = 0;  
 
     document.querySelector("#arBlack").style.opacity = 0;
     document.querySelector("#arBlack").style.zIndex = 0;
